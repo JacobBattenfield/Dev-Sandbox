c8c9c8c4d7a50d2994cf3f002d751d552afc16ee
jaybat-code <jacobbattenfielddev@gmail.com>
2024-10-12 13:04:04 -0500

    A Big Mess

---
+++ LeetCodeChallenges/Problems.cs
@@ -0,0 +1,42 @@
+class Problems{
+
+    public static int NthFibNum(int n){
+        if(n==0){
+            return 0;
+        }
+        int cur = 1;
+        int prev = 0;
+        for(var i = +1;i<n;i++){
+            int temp = cur+prev;
+            prev = cur;
+            cur = temp;
+        }
+        return cur;
+    }
+    //returns true if it is a prime number and returns false if it is not a prime number
+    public static bool IsPrime(int n){
+        if(n==0||n==1){
+            return false;
+        }
+        for(int i = 2;i<n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+    return true;
+    }
+
+    public static int SumOfDigits(int n){
+        string SplitN = Convert.ToString(n);
+        char[] charArray = SplitN.ToCharArray();
+        int[] numArray = new int[charArray.Length];
+        for(int i = 0;i<charArray.Length;i++){
+            numArray[i] = Convert.ToInt32(charArray[i])-Convert.ToInt32('0');
+        }
+        int total = 0;
+        for(int i = 0;i<numArray.Length;i++){
+            total+=numArray[i];
+        }
+        return total;
+    }
+}
---
+++ LeetCodeChallenges/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program{
+    static void Main(){
+    
+        //Testcase 1
+        Console.WriteLine();
+        Console.WriteLine("Nth Fibonaci Number:");
+        Console.WriteLine("++++++++++++++++++++++++++++");
+        Console.WriteLine(Problems.NthFibNum(23));
+        Console.WriteLine(Problems.NthFibNum(15));
+        Console.WriteLine(Problems.NthFibNum(5));
+        Console.WriteLine("++++++++++++++++++++++++++++");
+        Console.WriteLine();
+        //Testcase 2
+        Console.WriteLine();
+        Console.WriteLine("Is N Prime?:");
+        Console.WriteLine("++++++++++++++++++++++++++++");
+        Console.WriteLine(Problems.IsPrime(3));
+        Console.WriteLine(Problems.IsPrime(15));
+        Console.WriteLine(Problems.IsPrime(1));
+        Console.WriteLine(Problems.IsPrime(5));
+        Console.WriteLine("++++++++++++++++++++++++++++");
+        Console.WriteLine();
+
+        //Testcase 3
+        Console.WriteLine();
+        Console.WriteLine("Sum Of Digits:");
+        Console.WriteLine("++++++++++++++++++++++++++++");
+        Console.WriteLine(Problems.SumOfDigits(33));
+        Console.WriteLine(Problems.SumOfDigits(11111111));
+        Console.WriteLine(Problems.SumOfDigits(32485));
+        Console.WriteLine("++++++++++++++++++++++++++++");
+        Console.WriteLine();
+    }
+}
